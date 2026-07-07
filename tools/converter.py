23e361c8423534b9147cb71057c7e71d617b51ae
louisXIV <130300063+louissiu198@users.noreply.github.com>
2026-07-07 15:24:22 +0800

    Update converter.py

--- tools/converter.py
+++ tools/converter.py
@@ -7,18 +7,6 @@ ws_json = input("[?] Json file name: ")
 with open(f'{ws_json}.json', 'r') as file:
     json_data = json.load(file)
 
-def decode_base64_msgpack_stream(b64_string):
-    raw_bytes = base64.b64decode(b64_string)
-
-    unpacker = msgpack.Unpacker(raw=False)
-    unpacker.feed(raw_bytes)
-
-    objects = []
-    for obj in unpacker:
-        objects.append(obj)
-
-    return objects
-
 id = random.randint(100, 999)
 data_values = []
 for entry in json_data['log']['entries']:
